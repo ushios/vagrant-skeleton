058ec7621b64d5ac7dea41969397667a02b0b372
Ushio Shugo <ushio.s@gmail.com>
2014-06-09 23:24:31 +0900

    Update boxes.rb
    fix

--- src/boxes.rb
+++ src/boxes.rb
@@ -6,6 +6,6 @@ require './settings/current.rb'
 
 $boxes = {
 	:precise64_12_04 => 'precise64_12_04',
-	:precise64_14_04 => 'precise64_14_04'
+	:precise64_14_04 => 'precise64_14_04',
 	:aws       => 'awsdummy'
 }
